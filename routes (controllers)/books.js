2d27eb1f4c292e4580471dd0a5049dfe689de34c
Danilo Loose <danloose14@gmail.com>
2022-11-13 20:01:41 -0300

    Books Create/index/New"

--- routes (controllers)/books.js
+++ routes (controllers)/books.js
@@ -8,14 +8,25 @@ const router = express.Router()
 // all book page
 router.get('/', async (req, res) => {
 
-    let searchOptions = {}
-    if (req.query.title != null && req.query.title !== '') {
-        searchOptions.title = new RegExp(req.query.title.trim(), 'i')
+    let query = Book.find()
+
+    console.log(req.query)
+    if (req.query.title != null && req.query.title != '') {
+        query = query.regex('title', new RegExp(req.query.title, 'i'))
+    }
+
+    if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
+        query = query.lte('publishDate', req.query.publishedBefore)
+    }
+
+    if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
+        query = query.gte('publishDate', req.query.publishedAfter)
     }
 
     try {
-        const books = await Book.find(searchOptions)
-        res.render('books/index', { books: books, searchOptions: searchOptions })
+        const books = await query.exec()
+        console.log(books);
+        res.render('books/index', { books: books, searchOptions: req.query })
     } catch (error) {
         res.redirect('/')
     }
